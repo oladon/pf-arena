3b09bf7b6f783a2f5bfe186c86833be115d0c3f0
Oladon <oladon+hg@gmail.com>
2015-08-02 13:32:23 -0600

    code clean-up and docstrings

--- pf-arena.lisp
+++ pf-arena.lisp
@@ -6,7 +6,7 @@
 
 (in-package :pf-arena)
 
-; Set up player & default settings
+;;; Set up player & default settings
 (defgameobject pfcc-player ()
   ((difficulty :initarg :difficulty
 	       :initform :normal
@@ -22,6 +22,7 @@
   ())
 
 (defmethod engine:output (message)
+  "The default output method for a console player"
   (declare (ignore history))
   (format t "~A" message))
 
@@ -59,12 +60,11 @@
 (defparameter *difficultymap* '((:easy . :max) (:normal . :half) (:hard . :rolled)))
 (defparameter *pc-class* 'console-player)
 (defparameter *pfccmonsters* '())
-; TODO: record crits? Damage dealt/taken?
+;; TODO: record crits? Damage dealt/taken?
 
-
-; Initialize Monsters
-;TODO: this list is incomplete!
+;; TODO: this monster list is incomplete!
 (defun init-monsters (monsters)
+  "Initialize the monsters list for a game"
   (let ((crs (remove-duplicates (mapcar #'engine:cr monsters) :test #'equal)))
     (mapcar #'(lambda (x) (cons x (list (remove-if-not #'(lambda (y) (= x (engine:cr y)))
 						       monsters))))
@@ -76,28 +76,32 @@
        (every #'(lambda (x) (or (alphanumericp x) (char= #\Space x))) name)
        (notevery #'(lambda (x) (char= #\Space x)) name)))
 
-; Get a name, set up save(?)
-; TODO: should get-player-name be a generic?
+;;; Get a name, set up save(?)
+;; TODO: should get-player-name be a generic?
 (defun get-player-name (player)
+  "Get the player's name"
   (let ((name (prompt-input player 
 			    :prompt "Enter your name: "
 			    :validation #'valid-name)))
     (setf (name player) name)))
 
-; Pick/make a monster for the PC to start as
+;;; Pick/make a monster for the PC to start as
 (defun make-form (form)
+  "Make an instance of a monster form based on a prototypal form"
   (when form
     (make-instance (engine:make-anonymous 'engine:game-class 
 					  (list (class-of form)
 						(find-class 'engine:npc)))
 		   :name (engine:name form))))
 
-(defgeneric update-form (currentform newform))
+(defgeneric update-form 
+    "Update the player with a new form"
+  (currentform newform))
 (defmethod update-form ((it console-player) newform)
   (change-form it newform 'console-player))
 
 (defun change-form (oldform newform base)
-;  (setf (form-name oldform) (name newform))
+  "Change the classes of a player form, keeping the base player class"
   (change-class oldform 
 		(engine:make-anonymous 'engine:game-class
 				       (list (find-class base)
@@ -106,15 +110,18 @@
 		:hp-method (cdr (assoc (difficulty oldform) *difficultymap*))))
 
 (defun pick-starting-form (monsters)
+  "Choose a starting form for the player"
   (let ((forms (remove-if-not #'(lambda (x) (melee x)) (cadar monsters))))
     (nth (random (length forms)) forms)))
 
 (defun set-starting-form (player monsters)
+  "Set the player's starting form"
   (update-form player (pick-starting-form monsters)))
 
-; Pick/make a monster for the PC to face off against first
-; TODO: copy it, don't change the global list
+;; Pick/make a monster for the PC to face
+;; TODO: copy it, don't change the global list
 (defun find-opponent (pc-form monsters)
+  "Choose an opponent (monster) for the PC to face"
   (let* ((pc-cr (engine:cr pc-form))
 	 (valid-opponents (append (cadr (assoc pc-cr monsters)) 
 				  (cadr (find pc-cr monsters :test #'< :key #'car)))))
@@ -123,9 +130,10 @@
       (nth (random (length valid-opponents))
 	   valid-opponents))))
 
-; Fight!
-; TODO: determine who won, save history, etc.
+;;; Fight!
+;; TODO: save history, etc.
 (defun run-fight (pc badguys)
+  "Run a fight between a player (pc) and one or more opponents"
   (multiple-value-bind (opponents events) 
       (if (listp badguys) 
 	  (apply #'run-combat (cons pc badguys))
@@ -134,6 +142,7 @@
     (values opponents events)))
 
 (defun player-wins? (pc history)
+  "Determine from the history whether or not the player won a fight"
   (if (engine:conditions-active? pc '(engine:condition-dying 
 				      engine:condition-dead 
 				      engine:condition-unconscious) 
@@ -144,18 +153,21 @@
       t))
 
 (defun update-stats (player opponents history)
-  (mapcar #'(lambda (creature) (when (not (engine:conditions-active? player '(engine:condition-dying 
-									      engine:condition-dead 
-									      engine:condition-unconscious) 
-								     history :test :some))
-				 (progn (setf (kills player) (append (list creature) (kills player)))
-					(incf (xp-earned player) (xp creature)))))
+  "Update the player's kills and experience"
+  (mapcar #'(lambda (creature) 
+	      (when (not (engine:conditions-active? player '(engine:condition-dying 
+							     engine:condition-dead 
+							     engine:condition-unconscious) 
+						    history :test :some))
+		(progn (setf (kills player) (append (list creature) (kills player)))
+		       (incf (xp-earned player) (xp creature)))))
 	  (if (listp opponents)
 	      opponents
 	      (list opponents))))
 
-; In case of victory, offer PC new monster to control
+;; In case of victory, offer PC new monster to control
 (defun offer-forms (pc forms)
+  "Offer the form just defeated to the player"
   (let* ((existing-form (format nil "Keep current form (~A)" (form-name pc)))
 	 (form-choice (select pc 
 			      (append (if (listp forms) forms (list forms)) 
@@ -167,24 +179,12 @@
 							 (string-capitalize (form-name x))
 							 x))
 			      :prompt "Take a new form?")))
-;    (engine:format-output "You chose ~A.~%" (if (equal form-choice "Keep current form")
-;						(format nil "to keep your current form (~A)" (form-name pc))
-;						(pretty-print form-choice)))
     (cond ((equal form-choice existing-form) nil)
 	  ((equal form-choice "Restart") "Restart")
 	  (t form-choice))))
 
-; Fix PC up if PC kept form
-;; Deprecated as of HG 130
-;(defgeneric restore-creature (creature))
-;(defmethod restore-creature (creature)
-;  (engine:clear-conditions creature)
-;  (engine:restore-hp creature))
-
-; In case of PC death, go back to beginning (record score [fights won, final form])
-
-; Repeat
 (defun setup-game (player)
+  "Set up a new game"
   (when (or (not (name player))
 	    (string= "" (name player)))
     (get-player-name player))
@@ -194,10 +194,12 @@
 
 
 (defun setup-console-game (&optional player)
+  "Set up a game for play via the console"
   (let ((player (or player (make-instance 'console-player))))
     (setup-game player)))
 
 (defun run-round (player)
+  "Run one round of the fight"
   (let* ((opponent-forms (find-opponent player *pfccmonsters*))
 	 (opponents (if (listp opponent-forms)
 			(mapcar #'make-form opponent-forms)
@@ -210,25 +212,19 @@
 				      opponent-forms)))
 	    (when (and newform (not (stringp newform)))
 	      (update-form player newform))
-; TODO: figure out best way to restart game on death
-;	    (let ((again (select pc (list "Again!") 
-;				 :prompt "Play Again?")))
-;	      (when again
-;		(set-starting-form pc *pfccmonsters*)
-;		(restore-creature pc)
-;		(play pc))))
-;	  (output it (format nil "Participants: ~A~%" participants))
 	    (when (not (stringp newform))
-;	      (mapcar #'(lambda (x) (restore-creature (car x))) participants)
 	      player)))))))
 
 (defun play (player)
+  "Play the game (shouldn't be called directly)"
   (when player
     (loop for continue? = t
        for alive? = t
        do (setf alive? (run-round player))
        until (not alive?))))
 
+;; TODO: In case of PC death, go back to beginning (record score [fights won, final form])
 (defun play-console ()
+  "Play the game from the console"
   (let ((player (setup-console-game)))
     (play player)))
